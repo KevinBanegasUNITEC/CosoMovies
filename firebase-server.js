31405b2c308f0c1f50395c94d34db116d59bd6d7
AlexVila0204 <alberth@unitec.edu>
2024-06-15 22:10:02 -0600

    Firebase Auth LogIN

--- firebase-server.js
+++ firebase-server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 const connectFirebase = require('./config/firebase');
 const admin = require('firebase-admin');
@@ -6,27 +7,62 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/registrarse', async (req, res) => {
-    const {email, password} = req.body;
+app.post('/signup', async (req, res) => {
+    const {email} = req.body;
     console.log(`mostrar body ${req.body}`);
     try {
-        await admin.auth().createUser({
-            email,
-            password,
-            emailVerified: false,
-            disabled: false
+        
+        const userRecord = await admin.auth().getUserByEmail(email);
+        
+        //envia el uid a la base de datos
+        axios.post("http://localhost:3000/api/favorites/addUser", {
+            user: userRecord.uid,
+            favorites: []
         });
+
         res.status(201).json({
-            message: 'Usuario creado'
-        });z
+            message: 'user addded successfully',
+        });
     } catch (error) {
         res.status(400).json({
-            message: 'Error al crear el usuario',
+            message: 'error at adding user',
             error
         });
     }
 });
 
+app.get('/login', async (req, res) => {
+    const {email, password} = req.body;
+    if (!email || !password) {
+        res.status(400).json({
+            message: 'Email y password son requeridos'
+        });
+    }
+    try {
+        const user = await admin.auth().getUserByEmail(email);
+        await admin.auth().createCustomToken
+        console.log(user.email, user.password);
+        if (user.password !== password) {
+            res.status(400).json({
+                message: 'Password incorrecto'
+            });
+        }
+        if (user.email !== email) {
+            res.status(400).json({
+                message: 'Email incorrecto'
+            });
+        }
+        res.status(200).json({
+            message: 'Usuario logeado',
+            user
+        });
+    } catch (error) {
+        res.status(400).json({
+            message: 'Error al logear el usuario',
+            error
+        });
+    }
+});
 
 
 const PORT =  process.env.PORT || 3001;
